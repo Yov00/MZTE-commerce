a7695f67d6373ee503f0d1c5dc9888b3c789840a
Yovelinski <yovelinski@pop-os.localdomain>
2020-04-17 19:06:17 +0300

    created sigin and manage auth

--- src/App.js
+++ src/App.js
@@ -8,18 +8,45 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SigninAndSingup from './pages/sign-in-and-sign-up/sign-in-and-sing-up';
 import HomePage from "./pages/homepage/homepage.component";
+import {auth} from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop" component={ShopPage} />
-        <Route exact path="/signin" component={SigninAndSingup} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      currentUser:null
+    }
+  }
+
+  unSubscribeFromAuth = null;
+
+  componentDidMount(){
+    this.unSubscribeFromAuth = auth.onAuthStateChanged(user=>{
+
+    });
+
+    auth.onAuthStateChanged(user=>{
+      this.setState({currentUser:user});
+    });
+  }
+
+  componentWillUnmount(){
+    this.unSubscribeFromAuth();
+  }
+
+
+  render(){
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/signin" component={SigninAndSingup} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
